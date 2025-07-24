5f8279d8d2e131d5b1fb548d326c117468fb0b01
Samsul <113889106+samsulpanjul@users.noreply.github.com>
2025-07-24 22:58:03 +0700

    feat: prioritize g1 race

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # Umamusume Auto Train
 
-Like the title says – this is a simple auto training for Umamusume.
+Like the title says, this is a simple auto training for Umamusume.
 
 This project is inspired by [shiokaze/UmamusumeAutoTrainer](https://github.com/shiokaze/UmamusumeAutoTrainer)
 
@@ -54,11 +54,35 @@ You can edit your configuration in `config.json`
 {
   "priority_stat": ["spd", "sta", "wit", "pwr", "guts"],
   "minimum_mood": "NORMAL",
-  "maximum_failure": 10
+  "maximum_failure": 10,
+  "prioritize_g1_race": false
 }
+
 ```
 
-Make sure the values match exactly as expected – typos might cause errors.
+`priority_stat` (array of strings)
+
+Determines the training stat priority. The bot will focus on these stats in the given order of importance.
+Accepted values: `"spd"`, `"sta"`, `"pwr"`, `"guts"`, `"wit"`
+
+`minimum_mood` (string)
+
+The lowest acceptable mood the bot will tolerate when deciding to train.
+Accepted values (case-sensitive): `"GREAT"`, `"GOOD"`, `"NORMAL"`, `"BAD"`, `"AWFUL"`
+
+**Example**: If set to `"NORMAL"`, the bot will train as long as the mood is `"NORMAL"` or better. If the mood drops below that, it’ll go for recreation instead.
+
+`maximum_failure` (integer)
+
+Sets the maximum acceptable failure chance (in percent) before skipping a training option.
+Example: 10 means the bot will avoid training with more than 10% failure risk.
+
+`prioritize_g1_race` (boolean)
+
+If `true`, the bot will prioritize G1 races except during July and August (summer).
+Useful for fan farming.
+
+Make sure the values match exactly as expected, typos might cause errors.
 
 ### Start
 
@@ -84,17 +108,18 @@ Starting from the second year, it switches to the second logic. If there’s no 
 - Some Uma that has special event/target goals (like Restricted Train Goldship or 2 G1 Race Oguri Cap) may not working.
 - OCR might misread failure chance (e.g., reads 33% as 3%) and proceeds with training anyway.
 - Sometimes it misdetects debuffs and clicks the infirmary unnecessarily (not a big deal).
-- Automatically picks the top option during chain events. Be careful with Acupuncture event – it always picks the top option.
+- Automatically picks the top option during chain events. Be careful with Acupuncture event, it always picks the top option.
 - If you bring a friend support card (like Tazuna/Aoi Kiryuin) and do recreation, the bot can't decide whether to date with the friend support card or the Uma.
+- When `prioritize_g1_race` is set to `true`, the bot will always prioritize racing, even if your energy is low or you've already done 3 or more consecutive races.
 
 ### TODO
 
-- Prioritize G1 races for farm fans
+- ~~Prioritize G1 races for farm fans~~
 - Auto-purchase skills
 - Automate Claw Machine event
-- Add stat target feature – if a stat already hits the target, skip training that one
+- Add stat target feature, if a stat already hits the target, skip training that one
 
 ### Contribute
 
 If you run into any issues or something doesn’t work as expected, feel free to open an issue.
-Contributions are also very welcome — I would truly appreciate any support to help improve this project further.
+Contributions are also very welcome, I would truly appreciate any support to help improve this project further.
--- core/execute.py
+++ core/execute.py
@@ -7,13 +7,14 @@ pyautogui.useImageNotFoundException(False)
 from core.state import check_support_card, check_failure, check_turn, check_mood, check_current_year, check_criteria
 from core.logic import do_something
 from utils.constants import MOOD_LIST
-from core.recognizer import is_infirmary_active
+from core.recognizer import is_infirmary_active, match_template
 from utils.scenario import ura
 
 with open("config.json", "r", encoding="utf-8") as file:
   config = json.load(file)
 
 MINIMUM_MOOD = config["minimum_mood"]
+PRIORITIZE_G1_RACE = config["prioritize_g1_race"]
 
 def click(img, confidence = 0.8, minSearch = 2, click = 1, text = ""):
   btn = pyautogui.locateCenterOnScreen(img, confidence=confidence, minSearchTime=minSearch)
@@ -56,7 +57,7 @@ def check_training():
       time.sleep(0.1)
   
   pyautogui.mouseUp()
-  click("assets/buttons/back_btn.png")
+  click(img="assets/buttons/back_btn.png")
   return results
 
 def do_train(train):
@@ -72,10 +73,11 @@ def do_rest():
     pyautogui.click(rest_summber_btn)
 
 def do_recreation():
-  click("assets/buttons/recreation_btn.png")
+  click(img="assets/buttons/recreation_btn.png")
 
 def do_race():
   click(img="assets/buttons/races_btn.png", minSearch=10)
+  click(img="assets/buttons/ok_btn.png", minSearch=0.7)
 
   found = race_select()
   if not found:
@@ -88,9 +90,10 @@ def do_race():
   return True
 
 def race_day():
-  click("assets/buttons/race_day_btn.png", minSearch=10)
-
-  click(img="assets/ui/match_track.png", minSearch=1)
+  click(img="assets/buttons/race_day_btn.png", minSearch=10)
+  
+  click(img="assets/buttons/ok_btn.png", minSearch=0.7)
+  time.sleep(0.5)
 
   for i in range(2):
     click(img="assets/buttons/race_btn.png", minSearch=2)
@@ -102,26 +105,53 @@ def race_day():
 
 def race_select():
   pyautogui.moveTo(x=560, y=680)
-  
-  for i in range(10):
-    match_race = pyautogui.locateCenterOnScreen("assets/ui/match_track.png", confidence=0.8, minSearchTime=1)
-    if match_race:
-      print("[INFO] Race found.")
-      pyautogui.moveTo(match_race, duration=0.2)
-      pyautogui.click(match_race)
 
-      for i in range(2):
-        race_btn = pyautogui.locateCenterOnScreen("assets/buttons/race_btn.png", confidence=0.8, minSearchTime=2)
-        if race_btn:
-          pyautogui.moveTo(race_btn, duration=0.2)
-          pyautogui.click(race_btn)
-          time.sleep(0.5)
+  time.sleep(0.2)
+
+  if PRIORITIZE_G1_RACE:
+    for i in range(2):
+      race_card = match_template("assets/ui/g1_race.png", threshold=0.8)
+
+      if race_card:
+        for x, y, w, h in race_card:
+          region = (x, y, 310, 90)
+          match_aptitude = pyautogui.locateCenterOnScreen("assets/ui/match_track.png", confidence=0.8, minSearchTime=0.7, region=region)
+          if match_aptitude:
+            print("[INFO] G1 race found.")
+            pyautogui.moveTo(match_aptitude, duration=0.2)
+            pyautogui.click()
+            for i in range(2):
+              race_btn = pyautogui.locateCenterOnScreen("assets/buttons/race_btn.png", confidence=0.8, minSearchTime=2)
+              if race_btn:
+                pyautogui.moveTo(race_btn, duration=0.2)
+                pyautogui.click(race_btn)
+                time.sleep(0.5)
+            return True
       
-      return True
-    pyautogui.scroll(-300)
-    time.sleep(0.3)
-  
-  return False
+      for i in range(4):
+        pyautogui.scroll(-300)
+    
+    return False
+  else:
+    for i in range(4):
+      match_aptitude = pyautogui.locateCenterOnScreen("assets/ui/match_track.png", confidence=0.8, minSearchTime=0.7)
+      if match_aptitude:
+        print("[INFO] Race found.")
+        pyautogui.moveTo(match_aptitude, duration=0.2)
+        pyautogui.click(match_aptitude)
+
+        for i in range(2):
+          race_btn = pyautogui.locateCenterOnScreen("assets/buttons/race_btn.png", confidence=0.8, minSearchTime=2)
+          if race_btn:
+            pyautogui.moveTo(race_btn, duration=0.2)
+            pyautogui.click(race_btn)
+            time.sleep(0.5)
+        return True
+      
+      for i in range(4):
+        pyautogui.scroll(-300)
+    
+    return False
 
 def race_prep():
   view_result_btn = pyautogui.locateCenterOnScreen("assets/buttons/view_results.png", confidence=0.8, minSearchTime=20)
@@ -175,6 +205,7 @@ def career_lobby():
     year = check_current_year()
     criteria = check_criteria()
     
+    print("\n=======================================================================================\n")
     print(f"Year: {year}")
     print(f"Mood: {mood}")
     print(f"Turn: {turn}\n")
@@ -198,6 +229,11 @@ def career_lobby():
       race_day()
       continue
 
+    # Mood check
+    if mood_index < minimum_mood:
+      do_recreation()
+      continue
+
     # Check if goals is not met criteria AND it is not Pre-Debut AND turn is less than 10 AND Goal is already achieved
     if criteria.split(" ")[0] != "criteria" and year != "Junior Year Pre-Debut" and turn < 10 and criteria != "Goal Achievedl":
       race_found = do_race()
@@ -208,21 +244,27 @@ def career_lobby():
         click(img="assets/buttons/back_btn.png", text="[INFO] Race not found. Proceeding to training.")
         time.sleep(0.5)
 
-    # Mood check
-    if mood_index < minimum_mood:
-      do_recreation()
-    else:
-      # Last, do training
+    year_parts = year.split(" ")
+    # If Prioritize G1 Race is true, check G1 race every turn
+    if PRIORITIZE_G1_RACE and len(year_parts) > 3 and year_parts[3] not in ["Jul", "Aug"]:
+      g1_race_found = do_race()
+      if g1_race_found:
+        continue
+      else:
+        # If there is no G1 race, go back and do training
+        click(img="assets/buttons/back_btn.png", text="[INFO] G1 race not found. Proceeding to training.")
+        time.sleep(0.5)
+    
+    # Last, do training
+    go_to_training()
+    time.sleep(0.5)
+    results_training = check_training()
+    
+    best_training = do_something(results_training)
+    if best_training:
       go_to_training()
       time.sleep(0.5)
-      results_training = check_training()
-      
-      best_training = do_something(results_training)
-      if best_training:
-        go_to_training()
-        time.sleep(0.5)
-        do_train(best_training)
-      else:
-        do_rest()
-    print("\n======================================================================\n")
+      do_train(best_training)
+    else:
+      do_rest()
     time.sleep(1)
